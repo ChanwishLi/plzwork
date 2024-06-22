f9b6e4343dcea9a97c2fffc2e43b7cdd9da9b7e5
Chanwish <Chanwishlim.2007@gmail.com>
2024-06-22 19:43:45 +0700

    idk

--- app/(root)/(routes)/find-a-guide/page.tsx
+++ app/(root)/(routes)/find-a-guide/page.tsx
@@ -341,7 +341,7 @@ const FindAGuidePage: React.FC = () => {
       <div className="Paragraph">
         <p className="gay">
          Hellow World! Hellow World!Hellow World!Hellow World!Hellow World!Hellow World!Hellow World!Hellow World!Hellow World!Hellow World!Hellow World!
-         Hellow World!Hellow World!Hellow World!Hellow World!Hellow World!
+         Hellow World!Hellow World!Hellow World!Hellow World!Hellow World!Hellow World!Hellow World!
         </p>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31007.52131419444!2d100.48188391205342!3d13.722072446648594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e298e8dba300b3%3A0x30100b25de24ea0!2sKhlong%20San%2C%20Bangkok%2010600!5e0!3m2!1sen!2sth!4v1719051759494!5m2!1sen!2sth"
